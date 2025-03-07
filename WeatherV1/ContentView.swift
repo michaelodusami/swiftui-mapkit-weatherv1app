5a6edd8d2d760ff97b10f3dc50fcf982bfb4dc30
Michael Odusami <kyrmikee@gmail.com>
2025-03-07 12:07:05 -0500

    Initial Commit

---
+++ WeatherV1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherV1
+//
+//  Created by Tise on 3/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherV1/WeatherV1App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherV1App.swift
+//  WeatherV1
+//
+//  Created by Tise on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
